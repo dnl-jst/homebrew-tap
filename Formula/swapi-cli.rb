a227ba3cc2e6d816bb66f3876c11134d5bbc6b5c
Daniel Jost <daniel.jost@jost-devops.de>
2025-08-02 16:27:31 +0200

    Update swapi-cli to v1.1.9
    Automatically updated by GitHub Actions

--- Formula/swapi-cli.rb
+++ Formula/swapi-cli.rb
@@ -5,8 +5,8 @@ class SwapiCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/dnl-jst/swapi-cli/releases/download/v1.1.7/swapi-cli-macos"
-      sha256 "1addbabeba7ecaf2d749da3947f6f7b9340159bdac79f0dfc71c713a9d3a6060"
+      url "https://github.com/dnl-jst/swapi-cli/releases/download/v1.1.9/swapi-cli-macos"
+      sha256 "597d1dd76d4cd4ea3bb365d7dd06ce581fac5baf49626b168fc8dff4f49679cc"
     else
       url "https://github.com/dnl-jst/swapi-cli/releases/download/v1.0.2/swapi-cli-macos"
       sha256 "9dc8e9a220c56f7be453e03e25474037bcb832a56792f4797388809d4908f363"
